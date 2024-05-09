d2e8ea31eca5e4827ea60a4006a4f7d046dd4270
Max Granberg <mg224em@student.lnu.se>
2024-05-09 18:55:36 +0200

    remove unnecessary code

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
-import AuthContext from './AuthContext';
+import React, { useState, useEffect } from 'react';
 
 function AdminDashboard() {
     const [teams, setTeams] = useState([]);
